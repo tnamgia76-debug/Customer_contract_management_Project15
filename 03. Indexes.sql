bcf5a2b216a4adef5aa423042b11e40c704c7376
Namtran <tnamgia76@gmail.com>
2026-04-10 21:51:56 +0700

    Update 03. Indexes.sql

--- 03. Indexes.sql
+++ 03. Indexes.sql
@@ -1,13 +1,10 @@
 USE mydb;
+DROP INDEX idx_customers_name ON Customers;
+DROP INDEX idx_contracts_customer_status ON Contracts;
+DROP INDEX idx_invoices_contract_status ON Invoices;
+DROP INDEX idx_payments_contract_date ON Payments;
 
-CREATE INDEX idx_customers_name
-ON Customers (CustomerName);
-
-CREATE INDEX idx_contracts_customer_status
-ON Contracts (CustomerID, Status);
-
-CREATE INDEX idx_invoices_contract_status
-ON Invoices (ContractID, InvoiceStatus);
-
-CREATE INDEX idx_payments_contract_date
-ON Payments (ContractID, PaymentDate);
+CREATE INDEX idx_customers_name ON Customers (CustomerName);
+CREATE INDEX idx_contracts_customer_status ON Contracts (CustomerID, Status);
+CREATE INDEX idx_invoices_contract_status ON Invoices (ContractID, InvoiceStatus);
+CREATE INDEX idx_payments_contract_date ON Payments (ContractID, PaymentDate);
